2bb575fce7d75fa4f766759c9a90cb2f808d7cb7
Henrique <henriquekezen.20251@poli.ufrj.br>
2026-01-30 18:48:16 -0300

    Adição botões

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <main className="min-h-screen bg-cream flex flex-col p-6 text-coffee">
       
       {/* --- 1. CABEÇALHO (Logo + Navegação Rápida) --- */}
-      <header className="flex flex-col md:flex-row items-center justify-between gap-6 mb-10">
+      <header className="flex flex-col md:flex-row items-center gap-6 mb-10">
         
         {/* Identidade Visual (Logo) */}
-          <div className="relative w-40 h-16 md:w-48 md:h-20"> 
+          <div className="relative w-40 h-16 md:w-52 md:h-20 ml-14"> 
             <Image
             src="/logo-cleangirl.png" 
             alt="CG Café & Co."
@@ -18,6 +18,22 @@ export default function Home() {
             priority 
           />
           </div>
+
+        {/* Botões de Navegação (Ao lado da Logo) */}
+        <nav className="flex gap-6">
+          <Link
+            href="/comidas"
+            className="px-11 py-2 rounded-full border border-coffee/20 text-coffee font-medium hover:bg-rose hover:text-white hover:border-transparent transition-all duration-300"
+          >
+            Comidas
+          </Link>
+          <Link
+            href="/bebidas"
+            className="px-11 py-2 rounded-full border border-coffee/20 text-coffee font-medium hover:bg-rose hover:text-white hover:border-transparent transition-all duration-300"
+          >
+            Bebidas
+          </Link>
+        </nav>
 
       </header>
     </main>
